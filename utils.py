d1c1470cd0d9151681b13b87291d3153b36d6af0
Yohahn <yohahnribeiro29@gmail.com>
2019-10-18 18:57:34 +0100

    Added utility function to find chessboard and draw the corners on an image.

--- utils.py
+++ utils.py
@@ -23,12 +23,12 @@ def find_cb_points(img, cb_size):
     return ret, corners
 
 
-def draw_cube_on_img(img_path, rvec, tvec, K, d):
+def draw_cube_on_img(img, rvec, tvec, K, d):
     """
     Helper function to ingest an image path , along with the rodrigues rotation,
     translation vectors and the camera calibration matrix and distortion coefficients
     to draw a cube on the image.
-    :param img_path: path to image
+    :param img: np array representing the image
     :param rvec: rodrigues rotation vector to get toimage plane
     :param tvec: translation vector to get to image plane
     :param K: Camera calibration matrix
@@ -41,8 +41,6 @@ def draw_cube_on_img(img_path, rvec, tvec, K, d):
     print(f"3D corners shape: {_3d_corners}")
     cube_corners_2d, _ = cv2.projectPoints(_3d_corners, rvec, tvec, K, d)
     print(f"2D corners shape: {cube_corners_2d}")
-
-    img = cv2.imread(img_path)
 
     red, blue, green = (0, 0, 255), (255, 0, 0), (0, 255, 0)  # red (in BGR)
     line_width = 1
@@ -66,3 +64,35 @@ def draw_cube_on_img(img_path, rvec, tvec, K, d):
     cv2.line(img, tuple(cube_corners_2d[7][0]), tuple(cube_corners_2d[4][0]), green, line_width)
 
     return img
+
+
+def find_chessboard_and_draw_cube(img, K, d, cb):
+    """
+    Helper function to ingest an image, find a chessboard on it and draw a cube if it exists
+    :param img: Np array for the image
+    :param K: Camera calibration matrix
+    :param d: Camera distortion vector
+    :param cb: Tuple containing the (col, row) for the chessboard
+    :return: None if not chessboard, else the image with the cube on it
+    """
+
+    _subpix_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
+
+    # get 3D image plane coords
+    objp = np.zeros((cb[0] * cb[1], 3), np.float32)
+    objp[:, :2] = np.mgrid[0:cb[0], 0:cb[1]].T.reshape(-1, 2)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(
+        gray, cb, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE
+    )
+
+    if ret:
+        print("Found corners")
+        cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), _subpix_criteria)
+        _, rvec, tvec, _ = cv2.solvePnPRansac(objp, corners, K, d)
+
+        print(f"Found rvec: {rvec.tolist()}, tvec: {tvec.tolist()}")
+
+        return draw_cube_on_img(img, rvec, tvec, K, d)
+    else:
+        return None
